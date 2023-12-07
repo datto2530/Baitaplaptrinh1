da4772226dcb985260d2b74b6215e42360a5ed92
Dattobias <152405844+datto2530@users.noreply.github.com>
2023-12-07 16:13:03 +0700

    Đã xong bài thời gian

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SystemTime
+{
+    class program
+    {
+        private static void Main(string[] args)
+        {
+            DateTime localTime = DateTime.Now;
+            Console.WriteLine("Thoi gian hien tai la: " + localTime);
+        }
+    }
+}
